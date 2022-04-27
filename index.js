222988301b31bb662461bf4356f747dd5fbb091a
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-28 00:47:11 +0600

    help me

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -25,6 +26,29 @@ async function run() {
             const cursor = db.find(query)
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        const eventtasks = client.db("Volunteer").collection("event-tasks")
+
+        app.post('/eventtasks', async (req, res) => {
+            const query = req?.body
+            console.log('adding new service', query);
+            const service = await eventtasks.insertOne(query)
+            res.send(service)
+        })
+
+        app.get('/events', async (req, res) => {
+            const query = {}
+            const cursor = eventtasks.find(query)
+            const events = await cursor.toArray()
+            res.send(events)
+        })
+
+        app.delete('/event/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await eventtasks.deleteOne(query)
+            res.send(result)
         })
     }
     finally {
